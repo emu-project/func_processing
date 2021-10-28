5e5f11a36da0dfef0f5867430b259db8211d9eab
Nathan Muncy <nmuncy@fiu.edu>
2021-10-28 18:52:38 -0400

    copy_data working

--- resources/afni/copy_data.py
+++ resources/afni/copy_data.py
@@ -9,7 +9,6 @@ def _copyfile_patched(fsrc, fdst, length=16 * 1024 * 1024):
         if not buf:
             break
         fdst.write(buf)
-    shutil.copyfile = _copyfile_patched
 
 
 def copy_data(prep_dir, work_dir, subj, sess, task, num_runs, tplflow_str):
@@ -38,13 +37,9 @@ def copy_data(prep_dir, work_dir, subj, sess, task, num_runs, tplflow_str):
     Returns
     -------
     file_dict : dict
-        dictionary of files copied from derivatives/fMRIprep
-        to derivatives/afni
-        {"anat": [file1, file2], "func": [fileA, fileB]}
-        e.g. {
-            "anat": ["struct_head.nii.gz"],
-            "func": [run-1_<task>_preproc.nii.gz],
-        }
+        files copied from derivatives/fMRIprep to derivatives/afni
+        e.g. {"brain-mask": "mask_brain.nii.gz"}
+
 
     Notes
     -----
@@ -60,6 +55,15 @@ def copy_data(prep_dir, work_dir, subj, sess, task, num_runs, tplflow_str):
     file output : tsv
         run-?_motion_all.tsv
     """
+
+    # switch for assigning file_dict keys
+    file_name_switch = {
+        "mask_brain.nii.gz": "brain-mask",
+        "mask_GM_prob.nii.gz": "gm-mask",
+        "mask_WM_prob.nii.gz": "wm-mask",
+        "struct_head.nii.gz": "t1-struct",
+    }
+
     # set vars, dict
     anat_str = f"{subj}_{sess}_{tplflow_str}"
     func_str = f"{subj}_{sess}_task-{task}"
@@ -75,7 +79,7 @@ def copy_data(prep_dir, work_dir, subj, sess, task, num_runs, tplflow_str):
         "func": {},
     }
 
-    # add preproc bold, confound TS to copy_dicts
+    # add preproc bold, confound TS to copy_dicts and file_name_switch
     for run in range(0, num_runs):
         h_run = f"run-{run+1}"
 
@@ -87,28 +91,25 @@ def copy_data(prep_dir, work_dir, subj, sess, task, num_runs, tplflow_str):
             f"{h_run}_motion_all.tsv"
         ] = f"{func_str}_{h_run}_desc-confounds_timeseries.tsv"
 
-    # copy data, fill file_dict if file copies
+        file_name_switch[f"{h_run}_{task}_preproc.nii.gz"] = f"epi-{h_run}"
+        file_name_switch[f"{h_run}_motion_all.tsv"] = f"mot-{h_run}"
+
+    # copy data
     file_dict = {}
     for scan_type in copy_dict:
-        file_dict[scan_type] = []
         source_dir = os.path.join(prep_dir, subj, sess, scan_type)
         for h_file in copy_dict[scan_type]:
             in_file = os.path.join(source_dir, copy_dict[scan_type][h_file])
             out_file = os.path.join(work_dir, h_file)
             if not os.path.exists(os.path.join(work_dir, h_file)):
+                print(f"{subj}: Copying {copy_dict[scan_type][h_file]} to {h_file}")
                 shutil.copyfile(in_file, out_file)
+
+            # write return dict
+            h_key = file_name_switch[h_file]
             if os.path.exists(os.path.join(work_dir, h_file)):
-                file_dict[scan_type].append(out_file)
+                file_dict[h_key] = h_file
+            else:
+                file_dict[h_key] = "Missing"
 
     return file_dict
-
-    # # 3dcopy data func data (prepended with tmp_*)
-    # tmp_list = [x for x in os.listdir(work_dir) if fnmatch.fnmatch(x, "tmp_*")]
-    # for tmp_file in tmp_list:
-    #     in_file = os.path.join(work_dir, tmp_file)
-    #     h_str = tmp_file.split(".")[0].split("_", 1)[1]
-    #     out_file = os.path.join(work_dir, f"{h_str}+tlrc")
-    #     if not os.path.exists(f"{out_file}.HEAD"):
-    #         h_cmd = f"3dcopy {in_file} {out_file} && rm {in_file}"
-    #         submit_hpc_subprocess(h_cmd)
-
