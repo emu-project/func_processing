c15ce3f4919877e11db4dbff144dc6c68692da99
Nathan Muncy <nmuncy@fiu.edu>
2021-11-04 11:39:58 -0400

    control_afni working

--- tests/test_afni_workflow.py
+++ tests/test_afni_workflow.py
@@ -20,12 +20,14 @@ task = "task-test"
 num_runs = 3
 tplflow_str = "space-MNIPediatricAsym_cohort-5_res-2"
 
-# %%
 afni_data = control_afni.control_preproc(
     deriv_dir, subj, sess, task, num_runs, tplflow_str
 )
 
+# %%
 decon_json = "sub-4002_decon_plan.json"
 afni_data = control_afni.control_deconvolution(
     deriv_dir, subj, sess, afni_data, decon_json
 )
+
+# %%
