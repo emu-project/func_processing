58ebbb8e23f10b3b066282b64dd90d739b44c664
Nathan Muncy <nmuncy@fiu.edu>
2021-11-04 14:48:52 -0400

    workflow/control_afni works for S2 (#5)
    * added copy_data, submit_jobs
    
    * copy_data working
    
    * process epi_blur working
    
    * masks module working
    
    * process.scale_epi working
    
    * added motion module
    
    * motion module done
    
    * added deconvolve/write_decon function
    
    * reorganized
    
    * added feedback
    
    * updated afni_data keys
    
    * deconvolve/write_decon working
    
    * deconvolve/run_reml module is working
    
    * cleaning
    
    * starting workflow
    
    * testing workflow
    
    * cleaning, workflow works on ses-S2
    
    * control_afni working

--- tests/test_afni_deconvolution.py
+++ tests/test_afni_deconvolution.py
@@ -6,16 +6,6 @@ generate foo_stats.REML_cmd script. REML script
 is then executed. Nuissance signal is also generated
 via a blurred white matter timeseries.
 
-Examples
---------
-python test_afni_deconvolution.py \\
-    -p sub-4002 \\
-    -s ses-S2 \\
-    -d /scratch/madlab/emu_test/derivatives \\
-    -a afni_data.json
-    -t decon_plan.json
-
-
 Notes
 -----
 Requires AFNI
@@ -49,101 +39,40 @@ from resources.afni import deconvolve
 
 
 # %%
-def get_args():
-    """Get and parse arguments"""
-    parser = ArgumentParser(description=__doc__, formatter_class=RawTextHelpFormatter)
-    requiredNamed = parser.add_argument_group("required named arguments")
-    requiredNamed.add_argument(
-        "-p",
-        "--part-id",
-        help="Participant ID (sub-1234)",
-        type=str,
-        required=True,
-    )
-    requiredNamed.add_argument(
-        "-s",
-        "--sess-str",
-        help="BIDS ses-string (ses-S2)",
-        type=str,
-        required=True,
-    )
-    requiredNamed.add_argument(
-        "-d",
-        "--deriv-dir",
-        help="/path/to/project/derivatives",
-        type=str,
-        required=True,
-    )
-    requiredNamed.add_argument(
-        "-a",
-        "--afni-data",
-        help="JSON string to find output of test_afni_preproc.py",
-        type=str,
-        required=True,
-    )
-    requiredNamed.add_argument(
-        "-t",
-        "--decon-plan",
-        help="JSON string for deconvolution plan",
-        type=str,
-        required=True,
-    )
+# For testing
+deriv_dir = "/scratch/madlab/emu_test/derivatives"
+subj = "sub-4002"
+sess = "ses-S2"
+timing_dir = os.path.join(deriv_dir, "timing_files", subj, sess)
 
-    if len(sys.argv) == 1:
-        parser.print_help(sys.stderr)
-        sys.exit(1)
+# setup directories
+afni_dir = os.path.join(deriv_dir, "afni")
+work_dir = os.path.join(afni_dir, subj, sess)
 
-    return parser
+# with open(os.path.join(work_dir, decon_json)) as jf:
+#     decon_plan = json.load(jf)
 
+afni_json = "afni_data.json"
+with open(os.path.join(work_dir, afni_json)) as jf:
+    afni_data = json.load(jf)
 
-# %%
-def main():
-    """Move data through AFNI pre-processing."""
+# write deconvolution
+dur = 2
+# for decon_str in decon_plan:
+#     tf_dict = decon_plan[decon_str]
+#     afni_data = deconvolve.write_decon(dur, decon_str, tf_dict, afni_data, work_dir)
 
-    # # For testing
-    deriv_dir = "/scratch/madlab/emu_test/derivatives"
-    subj = "sub-4002"
-    sess = "ses-S2"
-    timing_dir = os.path.join(deriv_dir, "timing_files", subj, sess)
+# make tf_dict
+time_list = [x for x in os.listdir(timing_dir)]
+time_list.sort()
+decon_str = time_list[0].split("_")[2]
+tf_dict = {}
+for time_file in time_list:
+    beh = time_file.split("_")[-1].split(".")[0]
+    tf_dict[beh] = os.path.join(timing_dir, time_file)
+afni_data = deconvolve.write_decon(dur, decon_str, tf_dict, afni_data, work_dir)
 
-    # # get passed arguments
-    # args = get_args().parse_args()
-    # subj = args.part_id
-    # sess = args.sess_str
-    # afni_json = args.afni_data
-    # decon_json = args.decon_plan
-    # deriv_dir = args.deriv_dir
-
-    # setup directories
-    afni_dir = os.path.join(deriv_dir, "afni")
-    work_dir = os.path.join(afni_dir, subj, sess)
-
-    # with open(os.path.join(work_dir, decon_json)) as jf:
-    #     decon_plan = json.load(jf)
-
-    afni_json = "afni_data.json"
-    with open(os.path.join(work_dir, afni_json)) as jf:
-        afni_data = json.load(jf)
-
-    # write deconvolution
-    dur = 2
-    # for decon_str in decon_plan:
-    #     tf_dict = decon_plan[decon_str]
-    #     afni_data = deconvolve.write_decon(dur, decon_str, tf_dict, afni_data, work_dir)
-
-    # make tf_dict
-    time_list = [x for x in os.listdir(timing_dir)]
-    time_list.sort()
-    decon_str = time_list[0].split("_")[2]
-    tf_dict = {}
-    for time_file in time_list:
-        beh = time_file.split("_")[-1].split(".")[0]
-        tf_dict[beh] = os.path.join(timing_dir, time_file)
-    afni_data = deconvolve.write_decon(dur, decon_str, tf_dict, afni_data, work_dir)
-
-    # run deconvolution
-    afni_data = deconvolve.run_reml(work_dir, afni_data)
-
-
-if __name__ == "__main__":
-    main()
+# run deconvolution
+afni_data = deconvolve.run_reml(work_dir, afni_data)
+with open(os.path.join(work_dir, "afni_data.json"), "w") as jf:
+    json.dump(afni_data, jf)
