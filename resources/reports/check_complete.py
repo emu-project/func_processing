af5ebdde0576f30135121b13eb4749fccfcedd77
Nathan Muncy <nmuncy@fiu.edu>
2021-12-09 17:48:39 -0500

    added repo pull/push, bool check to check_complete

--- resources/reports/check_complete.py
+++ resources/reports/check_complete.py
@@ -16,47 +16,81 @@ import fnmatch
 
 # %%
 def check_preproc(proj_dir, pat_github_emu):
-    """Desc.
+    """Check for files in expected_dict.
 
-    Test.
+    In order to determine which participants need pre-processing,
+    and which have files on either the NAS or HPC, make a dataframe
+    of which participants have which pre-processed files.
+
+    This will use strings from expected_dict to create a dataframe. In
+    order to keep things synchronized, it will git clone/pull the repo
+    and add/commit/push the updated logs/completed_preprocessing.tsv.
+
+    Parameters
+    ----------
+    proj_dir : str
+        Path to BIDS-organized project directory, for
+        finding dset and derivatives
+    pat_github_emu : str
+        Personal Access Token to https://github.com/emu-project
+
+    Notes
+    -----
+    expected_dict should have the following organization:
+        - each key corresponds to a derivatives directory
+        - the value of each key is a list of tuples
+        - one tuple per file to search for
+        - tuple[0] is a string that matches a column in col_names
+            and a column of logs/completed_preprocessing.tsv
+        - tuple[1] is a string used to find the single file via glob
     """
 
     # # For testing
-    # # determine env, path
     # proj_dir = (
     #     "/home/data/madlab/McMakin_EMUR01"
     #     if platform.system() == "Linux"
     #     else "/Volumes/homes/MaDLab/projects/McMakin_EMUR01"
     # )
-    # repo_local = "/Users/nmuncy/Desktop/func_processing"
-    pat_github_emu = os.environ["TOKEN_GITHUB_EMU"]
+    # pat_github_emu = os.environ["TOKEN_GITHUB_EMU"]
 
-    # For each derivative directory in proj_dir, a key
-    # with associated tuples exist for file to be checked.
-    # One tuple should exist per file to check, and tuple[0]
-    # should contain the completed_tsv column name while tuple[1]
-    # contains the file string to search for
-    # Tuple[0] = column name of completed_tsv, tuple[1] = file name
-    #   e.g. "afni": [["intersect_1", "ses-S1_foo_desc-intersect_mask.nii.gz"]]
     expected_dict = {
         "afni": [
-            [
+            (
                 "intersect_1",
                 "ses-S1_space-MNIPediatricAsym_cohort-5_res-2_desc-intersect_mask.nii.gz",
-            ],
-            ["decon_1", "decon_task-study_UniqueBehs_stats_REML+tlrc.HEAD"],
-            [
+            ),
+            ("decon_1", "decon_task-study_UniqueBehs_stats_REML+tlrc.HEAD"),
+            (
                 "intersect_2",
                 "ses-S2_space-MNIPediatricAsym_cohort-5_res-2_desc-intersect_mask.nii.gz",
-            ],
-            ["decon_2", "decon_task-test_UniqueBehs_stats_REML+tlrc.HEAD"],
+            ),
+            ("decon_2", "decon_task-test_UniqueBehs_stats_REML+tlrc.HEAD"),
         ],
         "ashs": [
-            ["ashs_L", "left_lfseg_corr_usegray"],
-            ["ashs_R", "right_lfseg_corr_usegray"],
+            ("ashs_L", "left_lfseg_corr_usegray"),
+            ("ashs_R", "right_lfseg_corr_usegray"),
         ],
-        "reface": [["reface", "desc-reface"]],
+        "reface": [("reface", "desc-reface")],
     }
+
+    col_names = [
+        "subjID",
+        "intersect_1",
+        "intersect_2",
+        "decon_1",
+        "decon_2",
+        "ashs_L",
+        "ashs_R",
+        "reface",
+    ]
+
+    assert len(col_names) == 1 + sum(
+        [
+            len(expected_dict[x])
+            for x in expected_dict
+            if isinstance(expected_dict[x], list)
+        ]
+    ), "Unequal number of expected_dict values and col_names."
 
     # get, update repo
     repo_origin = f"https://{pat_github_emu}:x-oauth-basic@github.com/emu-project/func_processing.git"
@@ -80,16 +114,6 @@ def check_preproc(proj_dir, pat_github_emu):
 
     # make new completed_tsv if one does not exist
     if not os.path.exists(completed_tsv):
-        col_names = [
-            "subjID",
-            "intersect_1",
-            "intersect_2",
-            "decon_1",
-            "decon_2",
-            "ashs_L",
-            "ashs_R",
-            "reface",
-        ]
         df = pd.DataFrame(columns=col_names)
         df["subjID"] = subj_list
         df.to_csv(completed_tsv, index=False, sep="\t")
@@ -110,14 +134,16 @@ def check_preproc(proj_dir, pat_github_emu):
         for deriv in expected_dict:
             for h_count, _ in enumerate(expected_dict[deriv]):
 
-                # determine column index, file str, find file in derivatives
+                # determine column index, only continue if empty
                 ind_col = df_comp.columns.get_loc(expected_dict[deriv][h_count][0])
+                if pd.isna(df_comp.iloc[ind_row, ind_col]).bool() is False:
+                    continue
+
+                # determine file str, find file in derivatives, update df cell with creation time
                 deriv_str = expected_dict[deriv][h_count][1]
                 deriv_file = glob.glob(
                     f"{deriv_dir}/{deriv}/{subj}/**/*{deriv_str}*", recursive=True
                 )
-
-                # update df cell with creation time
                 if deriv_file:
                     df_comp.iloc[ind_row, ind_col] = time.strftime(
                         "%Y-%m-%d %H:%M:%S",
@@ -128,7 +154,7 @@ def check_preproc(proj_dir, pat_github_emu):
     df_comp.sort_values(by=["subjID"])
     df_comp.to_csv(completed_tsv, index=False, sep="\t")
 
-    # update repo
+    # update repo origin
     now = datetime.datetime.now()
     repo.git.add(completed_tsv)
     repo.index.commit(
