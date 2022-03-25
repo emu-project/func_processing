7db721f4b0f593ec788acf56d47f34f416b6a83a
Nathan Muncy <nmuncy@fiu.edu>
2022-03-25 15:34:25 -0400

    build dev (#33)

---
+++ setup.py
@@ -0,0 +1,5 @@
+# develop: python setup.py develop
+# install: python setup.py install
+from setuptools import setup, find_packages
+
+setup(name="func_processing", version="1.0", packages=find_packages())
